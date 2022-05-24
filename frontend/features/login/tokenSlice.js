fe9b7b13e0ce41a1db1005dbef9677148c20b8a6
charlie7002 <chaced@orange.fr>
2022-05-24 16:17:19 +0200

    test env

--- frontend/features/login/tokenSlice.js
+++ frontend/features/login/tokenSlice.js
@@ -1,19 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const tokenSlice = createSlice({
-  name: "token",
-  initialState: {
-    token: "",
-  },
-  reducers: {
-    addToken: (state, action) => {
-      state.token = action.payload;
-    },
-    removeToken: (state) => {
-      state.token = "";
-    },
-  },
+	name: 'token',
+	initialState: {
+		token: '',
+	},
+	reducers: {
+		addToken: (state, action) => {
+			state.token = action.payload;
+		},
+		removeToken: state => {
+			state.token = '';
+		},
+	},
 });
 
 export const { addToken, removeToken } = tokenSlice.actions;
 export default tokenSlice.reducer;
+
+//test
