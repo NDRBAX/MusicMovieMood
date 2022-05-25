f76304e084aa3dbc9374fdcee79e46eaeb98a5d6
charlie7002 <chaced@orange.fr>
2022-05-25 12:44:56 +0200

    filter en salle

--- frontend/data/filters.js
+++ frontend/data/filters.js
@@ -1,35 +1,35 @@
 export const filterMovieList = [
-  {
-    name: "mood",
-    img: require("../assets/images/movie_filter_mood_white.png"),
-    activeImg: require("../assets/images/movie_filter_mood_p.png"),
-  },
-  {
-    name: "public",
-    img: require("../assets/images/movie_filter_public_white.png"),
-    activeImg: require("../assets/images/movie_filter_public_p.png"),
-  },
-  {
-    name: "ou?",
-    img: require("../assets/images/movie_filter_where_white.png"),
-    activeImg: require("../assets/images/movie_filter_where_p.png"),
-  },
+	{
+		name: 'mood',
+		img: require('../assets/images/movie_filter_mood_white.png'),
+		activeImg: require('../assets/images/movie_filter_mood_p.png'),
+	},
+	{
+		name: 'public',
+		img: require('../assets/images/movie_filter_public_white.png'),
+		activeImg: require('../assets/images/movie_filter_public_p.png'),
+	},
+	{
+		name: 'ou?',
+		img: require('../assets/images/movie_filter_where_white.png'),
+		activeImg: require('../assets/images/cine.png'),
+	},
 ];
 
 export const filterMusicList = [
-  {
-    name: "mood",
-    img: require("../assets/images/movie_filter_mood_white.png"),
-    activeImg: require("../assets/images/movie_filter_mood_p.png"),
-  },
-  {
-    name: "ambiance",
-    img: require("../assets/images/movie_filter_mood_white.png"),
-    activeImg: require("../assets/images/movie_filter_mood_p.png"),
-  },
-  {
-    name: "genre",
-    img: require("../assets/images/movie_filter_mood_white.png"),
-    activeImg: require("../assets/images/movie_filter_mood_p.png"),
-  },
+	{
+		name: 'mood',
+		img: require('../assets/images/movie_filter_mood_white.png'),
+		activeImg: require('../assets/images/movie_filter_mood_p.png'),
+	},
+	{
+		name: 'ambiance',
+		img: require('../assets/images/movie_filter_mood_white.png'),
+		activeImg: require('../assets/images/movie_filter_mood_p.png'),
+	},
+	{
+		name: 'genre',
+		img: require('../assets/images/movie_filter_mood_white.png'),
+		activeImg: require('../assets/images/movie_filter_mood_p.png'),
+	},
 ];
--- frontend/features/movie/movieSlice.js
+++ frontend/features/movie/movieSlice.js
@@ -1,95 +1,98 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const movieSlice = createSlice({
-  name: "movie",
-  initialState: {
-    wishList: [
-      //example
-      // backdrop_path: '/4cWLRhub0yY9VJpdw0nqoTPYyiN.jpg',
-      // genres: [[Object], [Object], [Object], [Object]],
-      // id: 862,
-      // runtime': 81,
-      // title: 'Toy Story',
-      // year: 1995,
-    ],
-    blackList: [], //juste les ids
-    moviesFetch: [],
-    moviesNow: [],
-    moviesPopular: [],
-    moodFilter: "",
-    moodGenre: "",
-    publicFilter: false,
-    whereFilter: false,
-    displaySmiley: false,
-    movieClickForWatch: [],
-  },
-  reducers: {
-    //ADD MOVIE TO WISHLIST
-    addToWishlist: (state, { payload }) => {
-      !state.wishList.includes(payload.id) &&
-        (state.wishList = [...state.wishList, payload]);
-    },
-    //REMOVE ITEM FROM WISHLIST
-    removeFromWishlist: (state, { payload }) => {
-      state.wishList = state.wishList.filter((movie) => movie.id !== payload);
-    },
+	name: 'movie',
+	initialState: {
+		wishList: [
+			//example
+			// backdrop_path: '/4cWLRhub0yY9VJpdw0nqoTPYyiN.jpg',
+			// genres: [[Object], [Object], [Object], [Object]],
+			// id: 862,
+			// runtime': 81,
+			// title: 'Toy Story',
+			// year: 1995,
+		],
+		blackList: [], //juste les ids
+		moviesFetch: [],
+		moviesNow: [],
+		moviesPopular: [],
+		moodFilter: '',
+		moodGenre: '',
+		publicFilter: false,
+		whereFilter: false,
+		displaySmiley: false,
+		movieClickForWatch: [],
+	},
+	reducers: {
+		//ADD MOVIE TO WISHLIST
+		addToWishlist: (state, { payload }) => {
+			!state.wishList.includes(payload.id) && (state.wishList = [...state.wishList, payload]);
+		},
+		//REMOVE ITEM FROM WISHLIST
+		removeFromWishlist: (state, { payload }) => {
+			state.wishList = state.wishList.filter(movie => movie.id !== payload);
+		},
 
-    toggleSmiley: (state) => {
-      state.displaySmiley = !state.displaySmiley;
-    },
+		toggleSmiley: state => {
+			state.displaySmiley = !state.displaySmiley;
+		},
 
-    addMoodFilter(state, action) {
-      //plutot un toggle
-      const { name, genre } = action.payload;
-      state.moodFilter = name;
-      state.moodGenre = genre;
-    },
-    removeMoodFilter(state) {
-      state.moodFilter = "";
-    },
-    addPublicFilter(state) {
-      state.publicFilter = !state.publicFilter;
-      state.publicFilter === true && (state.moodGenre = "10751");
-      state.publicFilter === false && (state.moodGenre = "");
-    },
+		addMoodFilter(state, action) {
+			//plutot un toggle
+			const { name, genre } = action.payload;
+			state.moodFilter = name;
+			state.moodGenre = genre;
+			state.publicFilter = false;
+			state.whereFilter = false;
+		},
+		removeMoodFilter(state) {
+			state.moodFilter = '';
+		},
+		addPublicFilter(state) {
+			state.publicFilter = !state.publicFilter;
+			state.publicFilter === true && (state.moodGenre = '10751');
+			state.publicFilter === false && (state.moodGenre = '');
+			state.whereFilter = false;
+		},
 
-    addWhereFilter(state) {
-      state.whereFilter = !state.whereFilter;
-    },
-    addMovieFetch(state, { payload }) {
-      state.moviesFetch = payload;
-    },
-    addMoviePopularFetch(state, { payload }) {
-      state.moviesPopular = payload;
-    },
-    addToBlackList(state, { payload }) {
-      !state.blackList.includes(payload) &&
-        (state.blackList = [...state.blackList, payload]);
-    },
-    addMovieWatched(state, { payload }) {
-      !state.movieClickForWatch.includes(payload) &&
-        (state.movieClickForWatch = [...state.movieClickForWatch, payload]);
-    },
-    removeMovieWatched(state, { payload }) {
-      state.movieClickForWatch = state.movieClickForWatch.filter(
-        (movie) => movie.id !== payload
-      );
-    },
-  },
+		addWhereFilter(state) {
+			state.whereFilter = !state.whereFilter;
+		},
+		addMovieFetch(state, { payload }) {
+			state.moviesFetch = payload;
+		},
+		addMoviePopularFetch(state, { payload }) {
+			state.moviesPopular = payload;
+		},
+		addToBlackList(state, { payload }) {
+			!state.blackList.includes(payload) && (state.blackList = [...state.blackList, payload]);
+		},
+		addMovieWatched(state, { payload }) {
+			!state.movieClickForWatch.includes(payload) &&
+				(state.movieClickForWatch = [...state.movieClickForWatch, payload]);
+		},
+		removeMovieWatched(state, { payload }) {
+			state.movieClickForWatch = state.movieClickForWatch.filter(movie => movie.id !== payload);
+		},
+		addMovieNow(state, { payload }) {
+			state.moviesNow = payload;
+		},
+	},
 });
 
 export const {
-  addToWishlist,
-  removeFromWishlist,
-  toggleSmiley,
-  addMoodFilter,
-  addPublicFilter,
-  addWhereFilter,
-  removeMoodFilter,
-  addMovieFetch,
-  addMoviePopularFetch,
-  addToBlackList,
-  addMovieWatched,
-  removeMovieWatched,
+	addToWishlist,
+	removeFromWishlist,
+	toggleSmiley,
+	addMoodFilter,
+	addPublicFilter,
+	addWhereFilter,
+	removeMoodFilter,
+	addMovieFetch,
+	addMoviePopularFetch,
+	addToBlackList,
+	addMovieWatched,
+	removeMovieWatched,
+	addMovieNow,
 } = movieSlice.actions;
 export default movieSlice.reducer;
